55a74ffce724ef0ecc8998b0bb78e35bfc8ae74a
Rocky HIll <qbancoffee@yahoo.com>
2020-07-10 18:26:58 -0400

    removed auto generted javadocs from display properties gui

--- ImacG3DVDisplayProperties/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"imacg3dvdisplayproperties","l":"Displays"},{"p":"imacg3dvdisplayproperties","l":"ImacG3DVDisplayProperties"},{"p":"imacg3dvdisplayproperties","l":"SerialTrigger"}]
